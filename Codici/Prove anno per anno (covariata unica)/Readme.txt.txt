59ecb162278f5cc5e22f0529f2e2e425073b1ea1
Gabriele Mazza <lele.mazza90@gmail.com>
2014-11-28 16:00:12 +0100

    corretti i posti letto

---
+++ Codici/Prove anno per anno (covariata unica)/Readme.txt.txt
@@ -0,0 +1,12 @@
+Le cartelle contengono le prime applicazioni del modello.
+Tra le quadre c'è la risposta scelta:
+- PL: posti letto totali
+- TotalePC: rifiuti totali pro capite
+
+Gli altri termini sono covariate, quindi posti letto (che possono essere anche normalizzati,
+come posti letto ogni 1000 persone)
+
+In ogni esempio ci sono i plot della funzione stimata a diversi lambda, dove possibile gli IC anche corretti
+con Bonferroni, i massimi e i minimi assunti dalla funzione stimata (utili per la scala cromatica)
+
+Nel ciclo for sugli anni il plot di RgoogleMaps è ancora da perfezionare..
---
+++ Codici/Prove anno per anno (covariata unica)/[PL]/Prove anno per anno.R
@@ -0,0 +1,191 @@
+#PROVA ANNO PER ANNO
+source("Functions.R")
+source("2013_SSR_AllFunctions.R")
+load("Frontiera.RData")
+library(fda)
+library(rgl)
+library(RTriangle)
+library(sp)
+library(RgoogleMaps)
+
+##### ESTRAZIONE DEI PUNTI DI COMUNE #####
+
+#Carico i punti di comune
+Coord<-read.table("Coordinate.txt",header=T)
+names(Coord)
+#Leggo la risposta (rifiuti prodotto).. anche questa va ridotta
+Data<-read.table("Risposta.txt",header=T)
+names(Data)
+#Leggo il file con le covariate
+PostiLetto<-read.table("PostiLetto.txt",header=T)
+names(PostiLetto)
+
+##### CICLO PER GLI ANNI #####
+
+#Creo l'oggetto per la triangolazione
+#Sebbene i comuni cambino con il corso degli anni, eseguo la triangolazione con tutti i
+#581 comuni.
+#Poi assegnerò i dati solo a chi deve averli
+
+#Massimi e minimi (per i colori del plot)
+m<-NULL
+M<-NULL
+#Ogni anno raccolgo il massimo e il minimo della funzione stimata
+Years<-1997:2010
+#Creo anche due matrici in cui salvo gli intervalli di confidenza, corretti e non
+betahat<-NULL
+CI<-matrix(0,nrow=length(Years),ncol=3)
+CIsim<-matrix(0,nrow=length(Years),ncol=3)
+
+for (yearindex in 1:length(Years))
+{
+  year<-Years[yearindex]
+  print(paste("Sto calcolando l'anno ",year,sep=""))
+  #Prendo solo i dati che mi servono
+  #DataYear<-Data$TotalePC[Data$Anno==year]
+  #CodiciYear<-Data$Codice[Data$Anno==year]
+  #Nel caso dovessi eliminare il comune di Ariano nel Polesine
+  DataYear<-Data$TotalePC[Data$Anno==year & Data$Codice!=533]
+  CodiciYear<-Data$Codice[Data$Anno==year & Data$Codice!=533]
+  
+  if(year%%100<10)
+  {
+    name<-paste("PL0",year%%100,sep="")
+  } else
+  {
+    name<-paste("PL",year%%100,sep="")
+  }
+  
+  #Ora creo il vettore delle coordinate e della covariata corrispondenti
+  ComuniLat<-NULL
+  ComuniLon<-NULL
+  PL<-NULL
+  attach(PostiLetto)
+  for(i in 1:length(CodiciYear))
+  {
+    ComuniLat<-c(ComuniLat,Coord$Latitudine[Coord$Codice==CodiciYear[i]])
+    ComuniLon<-c(ComuniLon,Coord$Longitudine[Coord$Codice==CodiciYear[i]])
+    #Devo estrarre anche la covariata
+    PL<-c(PL,get(name)[Coord$Codice==CodiciYear[i]])
+  }
+  detach(PostiLetto)
+    #Latitudine è la y, Longitudine la x
+    #Ora creo la triangolazione
+    #Mi servono i punti
+    x<-c(ComuniLon,xBoundaries)
+    y<-c(ComuniLat,yBoundaries)
+    #La matrice di contorno
+    Boundaries<-NULL
+    for(i in (length(ComuniLon)+1):(length(x)-1))
+    {
+        Boundaries<-rbind(Boundaries,c(i,i+1))
+    }
+    Boundaries<-rbind(Boundaries,c(length(x),length(ComuniLon)+1))
+    #Creo la triangolazione
+    #Oggetto pslg
+    pslg_obj<-pslg(cbind(x,y),S=Boundaries)
+    #Creo la mesh
+    #Y dice di non aggiungere Steiner Points
+    #D dice di triangolare con Delaunay
+    mesh<-triangulate(pslg_obj,Y=TRUE,D=TRUE)
+    #Estrazione dei triangoli
+    Triang<-mesh$T
+    #Plot della triangolazione
+    #png(filename = paste("Triangolazione",year,".png",sep=""))
+    #plot(x,y,type="n")
+    #for (ne in 1:dim(Triang)[1])
+    #{
+    #    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+    #}
+    #dev.off()
+    
+    #Ora calcolo il modello
+    #No edge matrix
+    e = NULL
+    #Ordine
+    order=1
+    basisobj = create.FEM.basis(cbind(x,y), e, Triang, order)
+    #Creo l'oggetto fd
+    PLfd = fd(numeric(basisobj$nbasis),basisobj)
+    #Risposta
+    Z = matrix(0,nrow=length(PL),ncol=2)
+    Z[,1] = 1:length(PL)[1]
+    Z[,2] = PL
+    #Applico il modello
+    lambda = 10^0
+    PLsmooth = smooth.FEM.fd(Z,PLfd,lambda)
+    
+    #Massimi e minimi
+    fhat=PLsmooth$felsplobj$coef
+    m<-c(m,min(fhat))
+    M<-c(M,max(fhat))
+    #Ora ho bisogno del plot della soluzione
+    png(filename = paste("Funzione",year,".png",sep=""))
+    #Per lambda=10^2
+    #plot.FEM.2D(PLsmooth$felsplobj,zlimits=c(781,2430))
+    #Per lambda=10^1
+    #plot.FEM.2D(PLsmooth$felsplobj,zlimits=c(379,10947))
+    #Per lambda=10^0
+    plot.FEM.2D(PLsmooth$felsplobj,zlimits=c(-58,51339))
+    dev.off()
+    
+    #Per il googlemaps plot
+    #L <- list()
+    #for(i in 1:dim(mesh$T)[1])
+    #{
+    #    Point<-NULL
+    #Creo l'oggetto con i poligoni
+    #    for (j in 1:3)
+    #    {
+    #        Point<-rbind(Point,mesh$P[mesh$T[i,j],])
+    #    }
+    #    Point<-rbind(Point,mesh$P[mesh$T[i,1],])
+    #    str<-paste("TR",i,sep="")
+    #    tmp<-Polygon(Point)
+    #    Ps = Polygons(list(tmp), ID = str)
+    #    L[[paste0("TR", i)]]<-Ps
+    #}
+    #Oggetto di tipo SpatialPolygons
+    #SPs = SpatialPolygons(L)
+    #Ora dovrei creare l'oggetto con la valutazione della funzione nei baricentri dei triangoli.
+    #Ogni triangolo ha la funzione dipendente dal colore del suo baricentro.
+    #Creo un oggetto contenente i punti dei baricentri dei triangoli
+    #Barx<-NULL
+    #Bary<-NULL
+    #for(i in 1:dim(mesh$T)[1])
+    #{
+    #    xG=(mesh$P[mesh$T[i,1],1]+mesh$P[mesh$T[i,2],1]+mesh$P[mesh$T[i,3],1])/3
+    #    yG=(mesh$P[mesh$T[i,1],2]+mesh$P[mesh$T[i,2],2]+mesh$P[mesh$T[i,3],2])/3
+    #    Barx<-rbind(Barx,xG)
+    #    Bary<-rbind(Bary,yG)
+    #}
+    #Ora che ho tutti i baricentri, valuto la funzione nei baricentri
+    #zBar<-eval.FEM.fd(Barx,Bary,PLsmooth$felsplobj)
+    #Devo normalizzare questo vettore per creare dei color
+    #zCol<-(zBar-min(zBar))/(max(zBar)-min(zBar))
+    #from_col <- "yellow"
+    #to_col <- "red"
+    #val_col <- colorRampPalette(c(from_col,to_col))(length(zCol))
+    #Scarico la mappa
+    #rawdata <- data.frame(as.numeric(ComuniLon), as.numeric(ComuniLat))
+    #names(rawdata) <- c("lon", "lat")
+    #comuni <- as.matrix(rawdata)
+    #center <- rev(sapply(rawdata, mean))
+    #map <- GetMap(center=center, zoom=8)
+    #Plot dei poligoni
+    #png(filename = paste("Map",year,".png",sep=""))
+    #PlotOnStaticMap(map)
+    #PlotPolysOnStaticMap(map, SPs, val_col)
+    #dev.off()
+}
+
+#Plot dei massimi e dei minimi
+png(filename = "Massimi.png")
+plot(Years,M,main="Massimi della funzione stimata",pch=8,type='l')
+dev.off()
+png(filename = "Minimi.png")
+plot(Years,m,main="Minimi della funzione stimata",pch=8,type='l')
+dev.off()
+#Massimi e minimi per il grafico
+print(paste("Lower Bound: ",min(m),sep=""))
+print(paste("Upper Bound: ",max(M),sep=""))
