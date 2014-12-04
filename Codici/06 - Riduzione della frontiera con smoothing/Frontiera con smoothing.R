43f88966b1d206be55137fe0fe818bfe16c44cc8
Gabriele Mazza <lele.mazza90@gmail.com>
2014-12-04 11:43:48 +0100

    inizio di venezia

--- Codici/06 - Riduzione della frontiera con smoothing/Frontiera con smoothing.R
+++ Codici/06 - Riduzione della frontiera con smoothing/Frontiera con smoothing.R
@@ -64,14 +64,11 @@ for(i in 2:length(xVeneto_tot))
     s<-c(s,previous)  
 }
 
-plot(s,xVeneto_tot,type='l',main="xVeneto_tot")
-plot(s,yVeneto_tot,type='l',main="yVeneto_tot")
-
 #Ora devo fare smoothing di queste due funzioni
 #Come si fa?
 
 #Quanti punti fisso per la definizione del confine?
-N<-30
+N<-150
 #Nel range di ascissa curvilinea fisso N punti
 Val<-seq(0,max(s),by=max(s)/N)
 #Decido di usare delle splines cubiche
@@ -79,6 +76,8 @@ m<-4
 
 
 ##### REGRESSION SPLINES #####
+
+##### ANALISI DEL MIGLIOR nbasis #####
 
 #Scelgo come upper bound il numero di punti della valutazione per il numero di basi con
 #cui comporre la frontiera
@@ -120,7 +119,7 @@ for(nbasis in (m+1):N)
     y<-c(Comuni$Latitudine,yVeneto)
     #Creo i Boundaries
     Boundaries<-NULL
-    for(i in (length(Comuni$Longitudine)+1):length(x))
+    for(i in (length(Comuni$Longitudine)+1):(length(x)-1))
     {
         Boundaries<-rbind(Boundaries, c(i,i+1))
     }
@@ -175,8 +174,12 @@ png(filename = "Best.png")
 plot(best_vect,type='l')
 dev.off()
 
-#Si trova che il meglio si ha con nbasis 94
-nbasis<-140
+##### RISULTATI #####
+
+#Se io scelgo di avere 150 punti, succede che il miglior nbasis è 136 con la distanza,
+#ma a livello di quanti comuni restano fuori e di quanti triangoli vengono tolti, è meglio
+#nbasis 145
+nbasis<-145
 basis = create.bspline.basis(c(0,max(s)), nbasis, m)
 xsmooth = smooth.basis(argvals=s, y=xVeneto_tot, basis)
 ysmooth = smooth.basis(argvals=s, y=yVeneto_tot, basis)
@@ -209,7 +212,7 @@ x<-c(Comuni$Longitudine,xVeneto)
 y<-c(Comuni$Latitudine,yVeneto)
 #Creo i Boundaries
 Boundaries<-NULL
-for(i in (length(Comuni$Longitudine)+1):length(x))
+for(i in (length(Comuni$Longitudine)+1):(length(x)-1))
 {
     Boundaries<-rbind(Boundaries, c(i,i+1))
 }
@@ -250,7 +253,390 @@ if(sum(pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip)=
 points(Comuni$Longitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],Comuni$Latitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],col="red",pch=16)
 dev.off()
 
+##### COMUNI RIMASTI FUORI #####
+
+#Quali comuni restano fuori?
+Comuni$Comune[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0]
+
+#Ingrandisco nelle zone interessate dai comuni rimasti fuori
+
+#Inizio da lestebasse
+#Plot della triangolazione
+plot(x,y,col="white",xlim=c(11.2,11.3),ylim=c(45.85,45.95))
+for (ne in 1:dim(Triang)[1])
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+}
+#Li coloro
+for (ne in BorderTR)
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]),col="green")
+}
+points(Comuni$Longitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],Comuni$Latitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],col="red",pch=16)
+points(xVeneto_tot,yVeneto_tot,type='l',col="red")
+#Se allargo un pò la regione, ci sta dentro anche questo comune
+#identify(xVeneto,yVeneto,pos=T,plot=T)
+xVeneto[127]=11.26
+yVeneto[127]=45.92
+points(xVeneto[127],yVeneto[127])
+
+
+#Ora il comune di Melara
+#Plot della triangolazione
+plot(x,y,col="white",xlim=c(11.10,11.30),ylim=c(44.95,45.15))
+for (ne in 1:dim(Triang)[1])
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+}
+#Li coloro
+for (ne in BorderTR)
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]),col="green")
+}
+points(Comuni$Longitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],Comuni$Latitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],col="red",pch=16)
+points(xVeneto_tot,yVeneto_tot,type='l',col="red")
+#Se allargo un pò la regione, ci sta dentro anche questo comune
+#identify(xVeneto,yVeneto,pos=T,plot=T)
+xVeneto[105]=11.19
+yVeneto[105]=45.05
+points(xVeneto[105],yVeneto[105])
+xVeneto[106]=11.145
+yVeneto[106]=45.10
+points(xVeneto[106],yVeneto[106])
+
+#Riprovo tutto con queste modifiche
+#Controllo di intersezioni
+Intersect<-Intersections(xVeneto,yVeneto)
+Intersect
+#Oggetti completi
+x<-c(Comuni$Longitudine,xVeneto)
+y<-c(Comuni$Latitudine,yVeneto)
+#Creo i Boundaries
+Boundaries<-NULL
+for(i in (length(Comuni$Longitudine)+1):(length(x)-1))
+{
+    Boundaries<-rbind(Boundaries, c(i,i+1))
+}
+Boundaries<-rbind(Boundaries, c(length(x),length(Comuni$Longitudine)+1))
+#Ora triangolazione
+#Oggetto pslg
+pslg_obj<-pslg(cbind(x,y),S=Boundaries)
+#Creo la mesh
+#Y dice di non aggiungere Steiner Points
+#D dice di triangolare con Delaunay
+mesh<-triangulate(pslg_obj,Y=TRUE,D=TRUE)
+#Estrazione dei triangoli
+Triang<-mesh$T
+#Plot della triangolazione
+png(filename = "Triangolazione nella laguna.png")
+plot(x,y,col="white",xlim=c(12.1,12.6),ylim=c(45,45.6))
+for (ne in 1:dim(Triang)[1])
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+}
+#Controllo se la triangolazione ha triangoli con solo punti di bordo
+BorderTR<-BorderTriangles(mesh$T,Boundaries)
+BorderTR
+#Li coloro
+for (ne in BorderTR)
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]),col="green")
+}
+#Ci sono comuni esterni alla frontiera?
+PolyPoints<-cbind(xVeneto,yVeneto)
+if(sum(pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip)==length(Comuni$Longitudine))
+{
+    print("Tutti i comuni stanno dentro")
+} else
+{
+    print("Esistono comuni esterni alla frontiera")
+}
+points(Comuni$Longitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],Comuni$Latitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],col="red",pch=16)
+dev.off()
+
+#Quali comuni restano fuori?
+Comuni$Comune[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0]
+#Ottimo, solo chiogggia e venezia, ma sono le isole che devo ancora aggiungere
+
+#Ingrandisco nelle zone interessate dai comuni rimasti fuori
+#Inizio da lestebasse
+#Plot della triangolazione
+plot(x,y,col="white",xlim=c(11.2,11.3),ylim=c(45.85,45.95))
+for (ne in 1:dim(Triang)[1])
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+}
+#Li coloro
+for (ne in BorderTR)
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]),col="green")
+}
+points(Comuni$Longitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],Comuni$Latitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],col="red",pch=16)
+points(xVeneto_tot,yVeneto_tot,type='l',col="red")
+#Ottima correzione
+#Ora il comune di Melara
+#Plot della triangolazione
+plot(x,y,col="white",xlim=c(11.10,11.30),ylim=c(44.95,45.15))
+for (ne in 1:dim(Triang)[1])
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+}
+#Li coloro
+for (ne in BorderTR)
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]),col="green")
+}
+points(Comuni$Longitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],Comuni$Latitudine[pnt.in.poly(cbind(Comuni$Longitudine,Comuni$Latitudine),PolyPoints)$pip==0],col="red",pch=16)
+points(xVeneto_tot,yVeneto_tot,type='l',col="red")
+#Ottima correzione
+
+
+##### SMOOTHING SPLINES DI VENEZIA #####
+
+#Ora studio l'isola di venezia.
+#Anche qui devo cercare di ridurre al minimo i triangoli con vertici tutti di frontiera
+xVenezia_tot=xbound[labels==6]
+yVenezia_tot=ybound[labels==6]
+s<-0
+previous<-0
+
+for(i in 2:length(xVenezia_tot))
+{
+    #Aggiorno la distanza
+    previous<-previous+sqrt((xVenezia_tot[i]-xVenezia_tot[i-1])^2+(yVenezia_tot[i]-yVenezia_tot[i-1])^2)
+    s<-c(s,previous)  
+}
+N<-15
+#Nel range di ascissa curvilinea fisso N punti
+Val<-seq(0,max(s),by=max(s)/N)
+#Decido di usare delle splines cubiche
+m<-3
+
+#Scelgo come upper bound il numero di punti della valutazione per il numero di basi con
+#cui comporre la frontiera
+#Come scelgo l'ottimo del numero di basi?
+#Mi interessa che sia minimizzata la distanza totale quadratica euclidea
+#poligono reale di Venezia
+bestsum<-9999999999999
+best<-0
+sum_vect<-NULL
+best_vect<-NULL
+#Qui mi salvo un po' di risultati numerici
+sink(file = "ROutputVenezia.txt", append = FALSE)
+print("nbasis sum best Intersections Trinagoli")
+for(nbasis in (m+1):N)
+{
+    basis = create.bspline.basis(c(0,max(s)), nbasis, m)
+    xsmooth = smooth.basis(argvals=s, y=xVenezia_tot, basis)
+    ysmooth = smooth.basis(argvals=s, y=yVenezia_tot, basis)
+    xVenezia  = eval.fd(Val, xsmooth$fd)
+    yVenezia  = eval.fd(Val, ysmooth$fd)
+    png(filename = paste(nbasis,".png",sep=""))
+    plot(xVenezia_tot,yVenezia_tot,type='l',main=paste(nbasis,sep=""))
+    points(xVenezia,yVenezia,col="blue",type='l')
+    dev.off()
+    sum=0
+    for(i in 1:N)
+    {
+        sum<-sum+dist2Line(c(xVenezia[i],yVenezia[i]), cbind(xVenezia_tot,yVenezia_tot), distfun=SquareEuclideanDistance)[1]
+    }
+    if(sum<bestsum)
+    {
+        best<-nbasis
+        bestsum=sum
+    }
+    Intersect<-Intersections(xVenezia,yVenezia)
+    
+    #Oggetti completi
+    x<-c(Comuni$Longitudine[Comuni$Codice==425],xVenezia)
+    y<-c(Comuni$Latitudine[Comuni$Codice==425],yVenezia)
+    #Creo i Boundaries
+    Boundaries<-NULL
+    for(i in 2:(length(x)-1))
+    {
+        Boundaries<-rbind(Boundaries, c(i,i+1))
+    }
+    Boundaries<-rbind(Boundaries, c(length(x),2))
+    #Ora triangolazione
+    #Oggetto pslg
+    pslg_obj<-pslg(cbind(x,y),S=Boundaries)
+    #Creo la mesh
+    #Y dice di non aggiungere Steiner Points
+    #D dice di triangolare con Delaunay
+    mesh<-triangulate(pslg_obj,Y=TRUE,D=TRUE)
+    #Estrazione dei triangoli
+    Triang<-mesh$T
+    #Plot della triangolazione
+    png(filename = paste("Triangolazione a Venezia", nbasis, ".png", sep=""))
+    plot(x,y,col="white")
+    for (ne in 1:dim(Triang)[1])
+    {
+        polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+    }
+    #Controllo se la triangolazione ha triangoli con solo punti di bordo
+    BorderTR<-BorderTriangles(mesh$T,Boundaries)
+    BorderTR
+    #Li coloro
+    for (ne in BorderTR)
+    {
+        polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]),col="green")
+    }
+    #Ci sono comuni esterni alla frontiera?
+    points(Comuni$Longitudine[Comuni$Codice==425],Comuni$Latitudine[Comuni$Codice==425],col="red",pch=16)
+    
+    print(paste(
+        nbasis,
+        sum,
+        best,
+        dim(Intersect)[1],
+        length(BorderTR),
+        sep=" "))
+    sum_vect<-c(sum_vect,sum)
+    best_vect<-c(best_vect,best)
+    dev.off()
+}
+sink()
+png(filename = "Sum.png")
+plot(sum_vect,type='l')
+dev.off()
+png(filename = "Best.png")
+plot(best_vect,type='l')
+dev.off()
+
+#Troppi punti fanno in modo che si crei sempre un elevato numero di triangoli da eliminare
+#Quindi devo fare qualcosa di più semmplice
+#Scelgo nbasis 11
+nbasis<-11
+basis = create.bspline.basis(c(0,max(s)), nbasis, m)
+xsmooth = smooth.basis(argvals=s, y=xVenezia_tot, basis)
+ysmooth = smooth.basis(argvals=s, y=yVenezia_tot, basis)
+xVenezia  = eval.fd(Val, xsmooth$fd)
+yVenezia  = eval.fd(Val, ysmooth$fd)
+#Traccio ora i risultati
+png(filename = "Miglior smoothing di Venezia in x con regression splines.png")
+plot(s,xVenezia_tot,main="Smoothing di xVenezia_tot")
+points(Val,xVenezia,col="blue",type='l')
+dev.off()
+png(filename = "Miglior smoothing di Venezia in y con regression splines.png")
+plot(s,yVenezia_tot,main="Smoothing di yVenezia_tot")
+points(Val,yVenezia,col="blue",type='l')
+dev.off()
+png(filename = "Miglior smoothing di Venezia della regione con regression splines.png")
+plot(xVenezia_tot,yVenezia_tot,type='l',main="Nuova definizione della regione")
+points(xVenezia,yVenezia,col="blue",type='l')
+dev.off()
+
+#Controllo di intersezioni
+Intersect<-Intersections(xVenezia,yVenezia)
+Intersect
+
+#Oggetti completi
+x<-c(Comuni$Longitudine[Comuni$Codice==425],xVenezia)
+y<-c(Comuni$Latitudine[Comuni$Codice==425],yVenezia)
+#Creo i Boundaries
+Boundaries<-NULL
+for(i in 2:(length(x)-1))
+{
+    Boundaries<-rbind(Boundaries, c(i,i+1))
+}
+Boundaries<-rbind(Boundaries, c(length(x),2))
+#Ora triangolazione
+#Oggetto pslg
+pslg_obj<-pslg(cbind(x,y),S=Boundaries)
+#Creo la mesh
+#Y dice di non aggiungere Steiner Points
+#D dice di triangolare con Delaunay
+mesh<-triangulate(pslg_obj,Y=TRUE,D=TRUE)
+#Estrazione dei triangoli
+Triang<-mesh$T
+#Plot della triangolazione
+plot(x,y,col="white")
+for (ne in 1:dim(Triang)[1])
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+}
+#Controllo se la triangolazione ha triangoli con solo punti di bordo
+BorderTR<-BorderTriangles(mesh$T,Boundaries)
+BorderTR
+#Li coloro
+for (ne in BorderTR)
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]),col="green")
+}
+#Venezia
+points(Comuni$Longitudine[Comuni$Codice==425],Comuni$Latitudine[Comuni$Codice==425],col="red",pch=16)
+
+#Devo modificare questa visione dell'isola
+#identify(xVenezia,yVenezia,pos=T,plot=T)
+IDDelete<-c(1,5:9,13,14,16)
+#Alcun li devo modificare
+xVenezia[4]=12.345
+yVenezia[4]=45.443
+points(xVenezia[4],yVenezia[4])
+xVenezia[10]=12.342
+yVenezia[10]=45.433
+points(xVenezia[10],yVenezia[10])
+xVenezia[12]=12.312
+yVenezia[12]=45.434
+points(xVenezia[12],yVenezia[12])
+xVenezia=xVenezia[-IDDelete]
+yVenezia=yVenezia[-IDDelete]
+#Oggetti completi
+x<-c(Comuni$Longitudine[Comuni$Codice==425],xVenezia)
+y<-c(Comuni$Latitudine[Comuni$Codice==425],yVenezia)
+#Creo i Boundaries
+Boundaries<-NULL
+for(i in 2:(length(x)-1))
+{
+    Boundaries<-rbind(Boundaries, c(i,i+1))
+}
+Boundaries<-rbind(Boundaries, c(length(x),2))
+#Ora triangolazione
+#Oggetto pslg
+pslg_obj<-pslg(cbind(x,y),S=Boundaries)
+#Creo la mesh
+#Y dice di non aggiungere Steiner Points
+#D dice di triangolare con Delaunay
+mesh<-triangulate(pslg_obj,Y=TRUE,D=TRUE)
+#Estrazione dei triangoli
+Triang<-mesh$T
+#Plot della triangolazione
+plot(x,y,col="white")
+for (ne in 1:dim(Triang)[1])
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]))
+}
+#Controllo se la triangolazione ha triangoli con solo punti di bordo
+BorderTR<-BorderTriangles(mesh$T,Boundaries)
+BorderTR
+#Li coloro
+for (ne in BorderTR)
+{
+    polygon(c(x[Triang[ne,1]],x[Triang[ne,2]],x[Triang[ne,3]]),c(y[Triang[ne,1]],y[Triang[ne,2]],y[Triang[ne,3]]),col="green")
+}
+#Venezia
+points(Comuni$Longitudine[Comuni$Codice==425],Comuni$Latitudine[Comuni$Codice==425],col="red",pch=16)
+
+#Ora è bene unire venezia al resto della regione
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ##### SMOOTHING SPLINES CON PENALIZZAZIONE DELLA DERIVATA SECONDA #####
+
 #Se penalizzo anche con la derivata seconda, si trovano risultati poco aderenti alla
 #regione iniziale. Infatti, succede che la funzione viene resa più liscia possibile,
 #e quindi molte zone di contorno della regione sono appianate e molte "valli"
@@ -294,7 +680,7 @@ x<-c(Comuni$Longitudine,xVeneto)
 y<-c(Comuni$Latitudine,yVeneto)
 #Creo i Boundaries
 Boundaries<-NULL
-for(i in (length(Comuni$Longitudine)+1):length(x))
+for(i in (length(Comuni$Longitudine)+1):(length(x)-1))
 {
     Boundaries<-rbind(Boundaries, c(i,i+1))
 }
@@ -337,6 +723,7 @@ dev.off()
 
 
 ##### KERNEL SMOOTHING #####
+
 #Il kernel smoothing è una tecnica che scarto dopo alcuni tentativi
 #Infatti genera più di altri triangoli con lati di bordo
 #Questo probabilmente dovuto alla tecnica ma visto che li devo eliminare
@@ -365,7 +752,7 @@ x<-c(Comuni$Longitudine,xVeneto)
 y<-c(Comuni$Latitudine,yVeneto)
 #Creo i Boundaries
 Boundaries<-NULL
-for(i in (length(Comuni$Longitudine)+1):length(x))
+for(i in (length(Comuni$Longitudine)+1):(length(x)-1))
 {
     Boundaries<-rbind(Boundaries, c(i,i+1))
 }
