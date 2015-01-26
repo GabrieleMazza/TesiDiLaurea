00e9014b6efd864c5324cf9f193adbdd3b5f4d01
Gabriele Mazza <lele.mazza90@gmail.com>
2015-01-26 16:10:01 +0100

    Tutti i poligoni

---
+++ Codici/01 - Frontiere e triangolazioni/01 - Veneto/Poligoni del Veneto/Poligoni del Veneto.R
@@ -0,0 +1,39 @@
+# POLIGONI DEL VENETO
+
+#Scarico la frontiera
+#require(raster)
+#veneto =  subset(getData('GADM', country='ITA', level=1), NAME_1=="Veneto")
+#plot(veneto)
+#save(file="Veneto.RData",veneto)
+load("Veneto.RData")
+
+#La frontiera è memorizzata come una unione di 130 poligoni (a causa delle isole della)
+#laguna veneta)
+#Prendo solo la regione, ed è formata da un poligono di 20000 vertici.. lo riduco in base
+#alla tolleranza indicata
+#pIù bassa è la tolleranza, più punti sono tenuti per il poligono di confine
+
+tmp<- slot(veneto, 'polygons')
+sub.tmp <- slot(tmp[[1]],'Polygons') 
+order<-slot(tmp[[1]],'plotOrder')
+
+xbound<-NULL
+ybound<-NULL
+labels<-NULL
+for(i in 1:131)
+{
+    xbound<-c(xbound,sub.tmp[[order[i]]]@coords[,1])
+    ybound<-c(ybound,sub.tmp[[order[i]]]@coords[,2])
+    labels<-c(labels,rep(i,length(sub.tmp[[order[i]]]@coords[,1])))
+}
+
+length(xbound)
+length(ybound)
+length(labels)
+
+for(i in 1:131)
+{
+    png(filename=paste(i,".png",sep=""))
+    plot(xbound[labels==i],ybound[labels==i],type='l',lwd=2,main=paste("Poligono ",i,", ",length(ybound[labels==i])," vertici",sep=""),xlab="Longitudine",ylab="Latitudine")
+    dev.off()
+}
