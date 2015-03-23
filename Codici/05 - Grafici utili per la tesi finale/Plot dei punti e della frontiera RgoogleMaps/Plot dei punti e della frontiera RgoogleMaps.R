33bcdb4a3a996a97d1b1402117ec27f47bacf0a7
Gabriele Mazza <lele.mazza90@gmail.com>
2015-03-23 18:04:10 +0100

    Seconda versione della tesi

--- Codici/05 - Grafici utili per la tesi finale/Plot dei punti e della frontiera RgoogleMaps/Plot dei punti e della frontiera RgoogleMaps.R
+++ Codici/05 - Grafici utili per la tesi finale/Plot dei punti e della frontiera RgoogleMaps/Plot dei punti e della frontiera RgoogleMaps.R
@@ -11,7 +11,7 @@ ylim=c(min(y,ybound)-0.1,max(y,ybound)+0.1)
 Map<-GetMap.bbox(xlim, ylim, size = c(640, 640),MINIMUMSIZE = TRUE)
 
 png(filename="Comuni e frontiera.png")
-PlotOnStaticMap(Map,lon=xcom,lat=ycom,col="red",pch=20)
+PlotOnStaticMap(Map,lon=xcom,lat=ycom,col="black",pch=16)
 PlotOnStaticMap(Map,lon=xbound,lat=ybound,col="red",type='l',lwd=1,add=TRUE)
 dev.off()
 
@@ -21,6 +21,6 @@ ylim=c(45.2,45.5)
 Map<-GetMap.bbox(xlim, ylim, size = c(640, 640),MINIMUMSIZE = TRUE)
 
 png(filename="Comuni e frontiera zoom.png")
-PlotOnStaticMap(Map,lon=xcom,lat=ycom,col="red",pch=16)
+PlotOnStaticMap(Map,lon=xcom,lat=ycom,col="black",pch=16)
 PlotOnStaticMap(Map,lon=xbound,lat=ybound,col="red",type='l',add=TRUE)
 dev.off()
